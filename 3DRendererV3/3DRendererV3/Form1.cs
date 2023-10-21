89a19b1f005738637f71a3461c3d591eaa2d991d
PHOENIX33201 <96571288+PHOENIX33201@users.noreply.github.com>
2023-10-21 10:29:39 +0200

    Add files via upload

---
+++ 3DRendererV3/3DRendererV3/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Windows.Forms.VisualStyles;
+
+
+
+namespace _3DRendererV3
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap buffer;
+        private Graphics bufferGraphics;
+        private readonly System.Windows.Forms.Timer repaintTimer;
+        private const float FOCAL_LENGTH = 10000;
+        private const int INTERVAL = 10;
+        private const float TIME_SCALE = 0.01f;
+        internal static event Action Rotate;
+        internal static event Action<Graphics, Rectangle, float> Render;
+        private const int DISPLAY = 1;
+
+        public static List<(Point, Point, float, Color)> lines = new List<(Point, Point, float, Color)>();
+
+        #region initialization
+        public Form1()
+        {
+            InitializeBuffer();
+            InitializeComponent();
+
+            DoubleBuffered = true;
+
+            this.Paint += new PaintEventHandler(Form1_Paint);
+
+            // Initialize and configure the Timer
+            repaintTimer = new System.Windows.Forms.Timer();
+            repaintTimer.Interval = INTERVAL; // Set the interval in milliseconds (e.g., 10ms)
+            repaintTimer.Tick += RepaintTimer_Tick;
+
+            // Start the Timer
+            repaintTimer.Start();
+        }
+
+        private void InitializeBuffer()
+        {
+            buffer = new Bitmap(ClientSize.Width, ClientSize.Height);
+            bufferGraphics = Graphics.FromImage(buffer);
+            bufferGraphics.Clear(Color.Black); // Clear the buffer to a background color
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            if (buffer != null)
+            {
+                buffer.Dispose();
+                bufferGraphics.Dispose();
+            }
+
+            InitializeBuffer();
+            Refresh(); // Redraw the form
+        }
+
+        private void RepaintTimer_Tick(object sender, EventArgs e)
+        {
+            // Trigger a repaint of the form
+            this.Invalidate();
+        }
+        #endregion
+
+        #region Displays
+        private void Shapes()
+        {
+            Vector3 pivot = Vector3.Zero;
+            Vector3[] vertices;
+            (int, int)[] edges;
+            Quaternion rotation;
+
+
+            vertices = new Vector3[]
+            {
+                new Vector3( 30,  30,  30),
+                new Vector3(-30,  30,  30),
+                new Vector3(-30,  30, -30),
+                new Vector3( 30,  30, -30),
+
+                new Vector3( 30, -30,  30),
+                new Vector3(-30, -30,  30),
+                new Vector3(-30, -30, -30),
+                new Vector3( 30, -30, -30),
+            };
+
+            edges = new (int, int)[]
+            {
+                (0, 1), (4, 5), (0, 4),
+                (1, 2), (5, 6), (1, 5),
+                (2, 3), (6, 7), (2, 6),
+                (3, 0), (7, 4), (3, 7),
+            };
+
+            pivot = new Vector3(-100, 0, 0);
+            rotation = Quaternion.CreateFromAxisAngle(new Vector3(0, 1, 0.7f), 2f * TIME_SCALE);
+            new Object(pivot, vertices, edges, Color.Green, rotation);
+
+
+            vertices = new Vector3[]
+            {
+                new Vector3( 30, -30,  30),
+                new Vector3(-30, -30,  30),
+                new Vector3(-30, -30, -30),
+                new Vector3( 30, -30, -30),
+
+                new Vector3(  0,  30,   0),
+            };
+
+            edges = new (int, int)[]
+            {
+                (0, 1), (0, 4),
+                (1, 2), (1, 4),
+                (2, 3), (2, 4),
+                (3, 0), (3, 4),
+            };
+
+            pivot = new Vector3(100, 0, 0);
+            rotation = Quaternion.CreateFromAxisAngle(new Vector3(0, 1, 0), 2f * TIME_SCALE);
+            new Object(pivot, vertices, edges, Color.Purple, rotation).Rotate(Quaternion.CreateFromAxisAngle(new Vector3(0, 0, 1), (float)Math.PI / 2));
+        }
+
+        private void SolarSystem()
+        {
+            Vector3 pivot = Vector3.Zero;
+            Vector3[] vertices;
+            (int, int)[] edges;
+            Quaternion rotation;
+
+
+            vertices = new Vector3[]
+            {
+                new Vector3( 30,  30,  30),
+                new Vector3(-30,  30,  30),
+                new Vector3(-30,  30, -30),
+                new Vector3( 30,  30, -30),
+
+                new Vector3( 30, -30,  30),
+                new Vector3(-30, -30,  30),
+                new Vector3(-30, -30, -30),
+                new Vector3( 30, -30, -30),
+            };
+
+            edges = new (int, int)[]
+            {
+                (0, 1), (4, 5), (0, 4),
+                (1, 2), (5, 6), (1, 5),
+                (2, 3), (6, 7), (2, 6),
+                (3, 0), (7, 4), (3, 7),
+            };
+
+            pivot = new Vector3(0, -200, 0);
+            rotation = Quaternion.CreateFromAxisAngle(new Vector3(0, 1, 1f), 1.997f * TIME_SCALE);
+            new Object(null, vertices, edges, Color.Yellow, rotation);
+
+            rotation = Quaternion.CreateFromAxisAngle(new Vector3(-1, -1, -1), 10f / 365f * TIME_SCALE);
+            Object earthPivot = new Object(null, null, null, Color.Black, rotation);
+
+            vertices = new Vector3[]
+            {
+                new Vector3( 10,  10,  10),
+                new Vector3(-10,  10,  10),
+                new Vector3(-10,  10, -10),
+                new Vector3( 10,  10, -10),
+
+                new Vector3( 10, -10,  10),
+                new Vector3(-10, -10,  10),
+                new Vector3(-10, -10, -10),
+                new Vector3( 10, -10, -10),
+            };
+
+            pivot = new Vector3(100, 100, -100);
+            rotation = Quaternion.CreateFromAxisAngle(new Vector3(1, 1, 1), 10f * TIME_SCALE);
+            Object earth = new Object(pivot, vertices, edges, Color.Blue, rotation, earthPivot);
+
+            rotation = Quaternion.CreateFromAxisAngle(new Vector3(-1, -1, -1), 10f / (26f / 27f) * TIME_SCALE);
+            Object moonPivot = new Object(null, null, null, Color.Black, rotation, earth);
+
+            vertices = new Vector3[]
+            {
+                new Vector3( 5,  5,  5),
+                new Vector3(-5,  5,  5),
+                new Vector3(-5,  5, -5),
+                new Vector3( 5,  5, -5),
+
+                new Vector3( 5, -5,  5),
+                new Vector3(-5, -5,  5),
+                new Vector3(-5, -5, -5),
+                new Vector3( 5, -5, -5),
+            };
+
+            pivot = new Vector3(20, 20, 20);
+            rotation = Quaternion.CreateFromAxisAngle(new Vector3(1, 1, 1), 0);
+            new Object(pivot, vertices, edges, Color.White, rotation, moonPivot);
+        }
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            switch (DISPLAY)
+            {
+                case 0:
+                    Shapes();
+                    break;
+
+                case 1:
+                    SolarSystem();
+                    break;
+            }
+        }
+
+        private void RenderOnScreen(Graphics g)
+        {
+            Color BACKGROUND_COLOR = Color.Black;
+            using (Brush brush = new SolidBrush(BACKGROUND_COLOR))
+                bufferGraphics.FillRectangle(brush, this.ClientRectangle);
+
+            lines.Clear();
+            Render?.Invoke(bufferGraphics, this.ClientRectangle, FOCAL_LENGTH);
+            lines = lines.OrderByDescending(l => l.Item3).ToList();
+
+            using (Pen pen = new Pen(Color.Black, 3))
+            {
+                foreach ((Point, Point, float, Color) l in lines)
+                {
+                    pen.Color = l.Item4;
+                    bufferGraphics.DrawLine(pen, l.Item1, l.Item2);
+                }
+            }
+
+            g.DrawImage(buffer, 0, 0);
+        }
+
+        private void Movement()
+        {
+            Rotate?.Invoke();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics; // Take a Graphics object from the PaintEventArgs
+
+            Movement();
+
+            RenderOnScreen(g);
+        }
+    }
+}
---
+++ 3DRendererV3/3DRendererV3/Object.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _3DRendererV3
+{
+    internal sealed class Object
+    {
+        public Quaternion pivot;
+        private readonly Quaternion[] _vertices;
+        private readonly (int, int)[] _edges;
+        private readonly Color _color;
+        private Quaternion _constantRotation;
+        private Object _parent;
+        private Quaternion _rotation;
+
+        internal Object(Quaternion pivot, Quaternion[] vertices, (int, int)[] edges, Color color, Quaternion rotation, Object parent = null)
+        {
+            this.pivot = pivot;
+            _vertices = vertices;
+            _edges = edges;
+            _color = color;
+            _parent = parent;
+            _constantRotation = rotation;
+            _rotation = Quaternion.Identity;
+
+            Form1.Rotate += OnRotate;
+            Form1.Render += OnRender;
+        }
+
+        internal Object(Vector3? pivot, Vector3[] vertices, (int, int)[] edges, Color color, Quaternion rotation, Object parent = null)
+        {
+            this.pivot = pivot != null ? new Quaternion(pivot.Value.X, -pivot.Value.Y, pivot.Value.Z, 0) : new Quaternion(0, 0, 0, 0);
+            _vertices = vertices != null ? new Quaternion[vertices.Length] : new Quaternion[0]; if(vertices != null) for(int i = 0; i < vertices.Length; i++) _vertices[i] = new Quaternion(vertices[i].X, -vertices[i].Y, -vertices[i].Z, 0);
+            _edges = edges != null ? edges : new (int, int)[0];
+            _color = color;
+            _parent = parent;
+            _constantRotation = rotation;
+            _rotation = Quaternion.Identity;
+
+            Form1.Rotate += OnRotate;
+            Form1.Render += OnRender;
+        }
+
+        internal Object Copy()
+        {
+            return new Object(pivot, _vertices, _edges, _color, _constantRotation, _parent);
+        }
+
+        internal void RemoveEvents()
+        {
+            Form1.Rotate -= OnRotate;
+            Form1.Render -= OnRender;
+        }
+
+        internal Quaternion GetParentedPostition(Quaternion vertex)
+        {
+            Quaternion q = _rotation * vertex * Quaternion.Conjugate(_rotation);
+            q += pivot;
+            q = _parent?.GetParentedPostition(q) ?? q;
+            return q;
+        }
+
+        internal Quaternion GetGlobalRotation()
+        {
+            Quaternion q = _parent?.GetGlobalRotation() ?? Quaternion.Identity;
+            return _rotation * q;
+        }
+
+        internal void SetParent(Object parent)
+        {
+            if (parent == _parent) return;
+            if (parent == this) return;
+
+            pivot = _parent?.GetParentedPostition(pivot) ?? pivot;
+            _rotation = GetGlobalRotation();
+
+            _parent = parent;
+            if (parent == null) return;
+
+            pivot -= _parent?.GetParentedPostition(new Quaternion(0, 0, 0, 0)) ?? new Quaternion(0, 0, 0, 0);
+            Quaternion q = _parent?.GetGlobalRotation() ?? Quaternion.Identity;
+            pivot = Quaternion.Conjugate(q) * pivot * q;
+            _rotation /= q;
+        }
+
+        internal void SetRotation(Quaternion rotation)
+        {
+            _constantRotation = rotation;
+        }
+
+        internal void SetParentRelative(Object parent)
+        {
+            if (parent == null) return;
+            if (parent == _parent) return;
+            if (parent == this) return;
+
+            _parent = parent;
+
+            Quaternion q = _parent?.GetGlobalRotation() ?? Quaternion.Identity;
+            pivot = Quaternion.Conjugate(q) * pivot * q;
+            _rotation /= q;
+        }
+
+        internal void Rotate(Quaternion rotation)
+        {
+            _rotation *= rotation;
+        }
+
+        private void OnRotate()
+        {
+            _rotation = Quaternion.Conjugate(_constantRotation) * _rotation / _constantRotation;
+        }
+
+        private void OnRender(Graphics bufferGraphics, Rectangle rectangle, float focalLength)
+        {
+            (int, int, float)[] projectedVertices = new (int, int, float)[_vertices.Length];
+            for (int i = 0; i < _vertices.Length; i++)
+                projectedVertices[i] = CalculatePosition(_vertices[i]);
+
+            /*(int, int, float) projectedPivot = CalculatePosition(new Quaternion(0, 0, 0, 0));
+            int size = 6;
+            Brush brush = new SolidBrush(_color);
+            bufferGraphics.FillEllipse(brush, projectedPivot.Item1 - size / 2, projectedPivot.Item2 - size / 2, size, size);*/
+
+            using (Pen pen = new Pen(_color, 1))
+            {
+                foreach ((int, int) e in _edges)
+                {
+                    (int, int, float) d1 = projectedVertices[e.Item1];
+                    (int, int, float) d2 = projectedVertices[e.Item2];
+                    Point p1 = new Point(d1.Item1, d1.Item2);
+                    Point p2 = new Point(d2.Item1, d2.Item2);
+                    float renderOrder = (d1.Item3 + d2.Item3) / 2;
+                    Form1.lines.Add((p1, p2, renderOrder, _color));
+                }
+            }
+
+            (int, int, float) CalculatePosition(Quaternion vertex)
+            {
+                Quaternion q = GetParentedPostition(vertex);
+                float dividor = q.Z + focalLength;
+                float x = (focalLength * q.X / dividor) + rectangle.Width / 2;
+                float y = (focalLength * q.Y / dividor) + rectangle.Height / 2;
+
+                return ((int)x, (int)y, q.Z);
+            }
+        }
+    }
+}
